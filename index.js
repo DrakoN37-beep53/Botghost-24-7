1cd4b89526946ec84e5f82e5a6f38bf1b136349b
DrakoN37 <158779380+DrakoN37-beep53@users.noreply.github.com>
2024-10-20 18:21:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["EDIT THE STATUS", "🎮 Playing VALORANT"];
+const statusMessages = ["🐢 Cargando... por favor espera... unos 100 años más", "🎮 Haciendo speedrun de la vida (casi me mato en el tutorial)"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
